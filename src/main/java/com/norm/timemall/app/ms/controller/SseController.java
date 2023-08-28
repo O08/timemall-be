6c77601bd745baea18688fb1a986e415b5c1b838
O08 <47027217+O08@users.noreply.github.com>
2023-08-28 14:48:07 +0800

    code_20230828: sse crossorigin--dan

--- src/main/java/com/norm/timemall/app/ms/controller/SseController.java
+++ src/main/java/com/norm/timemall/app/ms/controller/SseController.java
@@ -3,7 +3,9 @@ package com.norm.timemall.app.ms.controller;
 import com.norm.timemall.app.base.entity.ResultVO;
 import com.norm.timemall.app.base.helper.SecurityUserHelper;
 import com.norm.timemall.app.ms.helper.SseHelper;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -14,11 +16,17 @@ public class SseController {
     /**
      * 用于创建连接
      */
+    @CrossOrigin
     @GetMapping("/api/v1/ms/sse/connect")
     public SseEmitter connect() {
         String userId = SecurityUserHelper.getCurrentPrincipal().getUserId();
         return SseHelper.connect(userId);
     }
+//    @CrossOrigin
+//    @GetMapping("/api/v1/ms/sse/connect/{userId}")
+//    public SseEmitter connect(@PathVariable String userId) {
+//        return SseHelper.connect(userId);
+//    }
 
 //    /**
 //     * 推送给所有人
@@ -45,10 +53,10 @@ public class SseController {
 //     * @param userid
 //     * @return
 //     */
-//    @GetMapping("/api/v1/ms/sse/push_one/{messsage}/{userid}")
-//    public CodeVO pushOne(@PathVariable(name = "message") String message, @PathVariable(name = "userid") String userid) {
+//    @GetMapping("/api/v1/ms/sse/push_one/{message}/{userid}")
+//    public ResultVO pushOne(@PathVariable(name = "message") String message, @PathVariable(name = "userid") String userid) {
 //        SseHelper.sendMessage(userid, message);
-//        return CodeVO.success("推送消息给" + userid);
+//        return ResultVO.success("推送消息给" + userid);
 //    }
 
     /**
