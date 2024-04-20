719abbffd70bcaaacbc7f7ad575cbc0c7d92252c
O08 <47027217+O08@users.noreply.github.com>
2024-04-20 16:21:53 +0800

    code_20240420: a1220 / a1219 bug fix--dan

--- src/main/java/com/norm/timemall/app/base/pojo/dto/BaseFetchCodeMappingDTO.java
+++ src/main/java/com/norm/timemall/app/base/pojo/dto/BaseFetchCodeMappingDTO.java
@@ -2,8 +2,11 @@ package com.norm.timemall.app.base.pojo.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class BaseFetchCodeMappingDTO {
+    @NotBlank(message = "codeType is required")
     private String codeType;
     private String itemCode;
 }
