76737844efe8d938b7496c96417779346a45f307
O08 <47027217+O08@users.noreply.github.com>
2024-10-07 15:40:29 +0800

    code_20241007: ppc etl loging --dan

--- src/main/java/com/norm/timemall/app/scheduled/PpcTaskETL.java
+++ src/main/java/com/norm/timemall/app/scheduled/PpcTaskETL.java
@@ -3,11 +3,13 @@ package com.norm.timemall.app.scheduled;
 import cn.hutool.core.date.DateUtil;
 import com.norm.timemall.app.scheduled.service.TaskPpcBillScheduleService;
 import com.norm.timemall.app.scheduled.service.TaskPpcLinkScheduleService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class PpcTaskETL {
     @Autowired
     private TaskPpcBillScheduleService taskPpcBillScheduleService;
@@ -17,21 +19,27 @@ public class PpcTaskETL {
 
     @Scheduled( cron = "0 0 5 ? * MON",zone = "Asia/Shanghai")
     public void autoPayPpcBill(){
-
+        log.info("autoPayPpcBill etl task start.....");
         taskPpcBillScheduleService.autoPayPpcBill();
+        log.info("autoPayPpcBill etl task end.....");
 
     }
     @Scheduled( cron = "0 0 1 ? * MON",zone = "Asia/Shanghai")
     public void generatePpcBill(){
 
-        String batch= DateUtil.lastWeek().toDateStr();
+        String batch= DateUtil.lastWeek().toDateStr().replace("-","");
+        log.info(batch+" batch taskPpcBillScheduleService etl task start.....");
         taskPpcBillScheduleService.generatePpcBill(batch);
+        log.info(batch+" batch taskPpcBillScheduleService etl task end.....");
 
-    }
+
+   }
     @Scheduled( cron = "0 0 1 * * ?",zone = "Asia/Shanghai")
     public void  refreshPpcLinkInfo(){
-
+        log.info("refreshPpcLinkInfo etl task start.....");
         taskPpcLinkScheduleService.refreshPpcLinkInfo();
+        log.info("refreshPpcLinkInfo etl task end.....");
+
 
     }
 
