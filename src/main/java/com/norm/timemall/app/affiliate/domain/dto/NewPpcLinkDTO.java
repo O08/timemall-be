fa07d8f4503d5821696f858c592258caedebaf31
O08 <47027217+O08@users.noreply.github.com>
2024-10-09 16:07:35 +0800

    code_20241009: bug fix--dan

--- src/main/java/com/norm/timemall/app/affiliate/domain/dto/NewPpcLinkDTO.java
+++ src/main/java/com/norm/timemall/app/affiliate/domain/dto/NewPpcLinkDTO.java
@@ -12,7 +12,7 @@ public class NewPpcLinkDTO {
     private String linkName;
 
     @NotBlank(message = "trackCode required")
-    @Pattern(regexp="^@\\w+$", message="trackCode字符不符合要求")
+    @Pattern(regexp="^\\w+$", message="trackCode字符不符合要求,支持大小写字母、数字、_")
     @Length(message = "trackCode range in {min}-{max}",min = 1,max = 32)
     private String trackCode;
 }
