beb690fd8ce397a97d985058bb3f958019331bc2
O08 <47027217+O08@users.noreply.github.com>
2024-10-09 14:33:22 +0800

    code_20241009: test bug fix -dan

--- src/main/java/com/norm/timemall/app/affiliate/domain/dto/NewPpcLinkDTO.java
+++ src/main/java/com/norm/timemall/app/affiliate/domain/dto/NewPpcLinkDTO.java
@@ -1,6 +1,7 @@
 package com.norm.timemall.app.affiliate.domain.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
@@ -11,6 +12,7 @@ public class NewPpcLinkDTO {
     private String linkName;
 
     @NotBlank(message = "trackCode required")
+    @Pattern(regexp="^@\\w+$", message="trackCode字符不符合要求")
     @Length(message = "trackCode range in {min}-{max}",min = 1,max = 32)
     private String trackCode;
 }
