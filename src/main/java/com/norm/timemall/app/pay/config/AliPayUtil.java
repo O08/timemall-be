06d3eae88a5cfd83f8c0d5a239c7583ed18a43ac
O08 <47027217+O08@users.noreply.github.com>
2025-06-04 15:13:34 +0800

    code_202500604: bug fix --dan

--- src/main/java/com/norm/timemall/app/pay/config/AliPayUtil.java
+++ src/main/java/com/norm/timemall/app/pay/config/AliPayUtil.java
@@ -2,6 +2,7 @@ package com.norm.timemall.app.pay.config;
 
 import com.alipay.api.AlipayConfig;
 import com.norm.timemall.app.base.config.env.EnvBean;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import java.io.IOException;
@@ -17,28 +18,28 @@ public class AliPayUtil {
         alipayConfig.setFormat("json");
 
         if(envBean.getSoftwareDevelopmentLifeCycle().equals("dev")){
-            alipayConfig.setAppCertPath(getServerPath()+"/dev/alipay-crt/appPublicCert.crt");
-            alipayConfig.setAlipayPublicCertPath(getServerPath()+"/dev/alipay-crt/alipayPublicCert.crt");
-            alipayConfig.setRootCertPath(getServerPath()+"/dev/alipay-crt/alipayRootCert.crt");
+            alipayConfig.setAppCertPath(getServerPath("dev/alipay-crt/appPublicCert.crt"));
+            alipayConfig.setAlipayPublicCertPath(getServerPath("dev/alipay-crt/alipayPublicCert.crt"));
+            alipayConfig.setRootCertPath(getServerPath("dev/alipay-crt/alipayRootCert.crt"));
         }
-        if(envBean.getSoftwareDevelopmentLifeCycle().equals("prd")){
-            alipayConfig.setAppCertPath(getServerPath()+"/alipay-crt/appPublicCert.crt");
-            alipayConfig.setAlipayPublicCertPath(getServerPath()+"/alipay-crt/alipayPublicCert.crt");
-            alipayConfig.setRootCertPath(getServerPath()+"/alipay-crt/alipayRootCert.crt");
+        if(envBean.getSoftwareDevelopmentLifeCycle().equals("prod")){
+            alipayConfig.setAppCertPath(getServerPath("alipay-crt/appPublicCert.crt"));
+            alipayConfig.setAlipayPublicCertPath(getServerPath("alipay-crt/alipayPublicCert.crt"));
+            alipayConfig.setRootCertPath(getServerPath("alipay-crt/alipayRootCert.crt"));
         }
 
         return alipayConfig;
     }
     public static String getAliPayPublicCertPath(EnvBean envBean){
         if(envBean.getSoftwareDevelopmentLifeCycle().equals("dev")){
-            return getServerPath()+"/dev/alipay-crt/alipayPublicCert.crt";
+            return getServerPath("dev/alipay-crt/alipayPublicCert.crt");
         }
 
-        return getServerPath()+"/alipay-crt/alipayPublicCert.crt";
+        return getServerPath("alipay-crt/alipayPublicCert.crt");
     }
-    private  static   String getServerPath(){
+    private  static   String getServerPath(String file){
         try {
-            return   new PathMatchingResourcePatternResolver().getResource("/").getFile().getPath();
+            return   new ClassPathResource(file).getURL().getPath();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
