eb38bf29968383df33295b7446dd995b2c008eb3
O08 <47027217+O08@users.noreply.github.com>
2025-06-23 17:28:52 +0800

    code_20250623: bug fix--dan

--- src/main/java/com/norm/timemall/app/team/service/impl/TeamTransServiceImpl.java
+++ src/main/java/com/norm/timemall/app/team/service/impl/TeamTransServiceImpl.java
@@ -6,8 +6,7 @@ import com.norm.timemall.app.base.entity.PageDTO;
 import com.norm.timemall.app.base.enums.FidTypeEnum;
 import com.norm.timemall.app.base.helper.SecurityUserHelper;
 import com.norm.timemall.app.team.domain.pojo.TeamTrans;
-import com.norm.timemall.app.team.mapper.TeamTransactionsMapper;
-import com.norm.timemall.app.team.mapper.TeamTransactionsQueryMapper;
+import com.norm.timemall.app.team.mapper.TeamTransactionsQueryMapperr;
 import com.norm.timemall.app.team.service.TeamTransService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,7 +14,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class TeamTransServiceImpl implements TeamTransService {
     @Autowired
-    private TeamTransactionsQueryMapper teamTransactionsQueryMapper;
+    private TeamTransactionsQueryMapperr teamTransactionsQueryMapper;
     @Override
     public IPage<TeamTrans>  findTrans(PageDTO dto) {
         IPage<TeamTrans>  page = new Page<>();
