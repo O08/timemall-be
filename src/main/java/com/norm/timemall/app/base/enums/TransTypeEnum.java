41dc22e70097db340dace8de37eedf70681d6cb2
O08 <47027217+O08@users.noreply.github.com>
2025-06-27 15:21:53 +0800

    code_20250627: bug fix--dan

--- src/main/java/com/norm/timemall/app/base/enums/TransTypeEnum.java
+++ src/main/java/com/norm/timemall/app/base/enums/TransTypeEnum.java
@@ -5,10 +5,10 @@ public enum TransTypeEnum {
     OTHER("2","其他"),
 
     TRANSFER("3","交易"),
-    TOPUP_OASIS("4","充值Oasis"),
-    OASIS_COLLECT_IN("5","oasis 收账"),
-    TOPUP_MPS_FUND("6","转帐到MPS fund"),
-    MPS_FUND_TRANSFER("7","合约基金支付转账"),
+    TOPUP_OASIS("4","部落赞助"),
+    OASIS_COLLECT_IN("5","部落收账"),
+    TOPUP_MPS_FUND("6","合约基金充值"),
+    MPS_FUND_TRANSFER("7","商单打款"),
     PLAN_ORDER_TRANSFER_TO_BRAND("8","平台单品打款"),
     PLAN_ORDER_PAY("9","单品付款"),
     REFUND("10","退款"),
@@ -16,7 +16,7 @@ public enum TransTypeEnum {
     MILLSTONE_AFFILIATE_PAY("12","特约账单佣金"),
     PLAN_ORDER_AFFILIATE_PAY("13", "单品佣金"),
     PPC_BILL("15","ppc 打款"),
-    OASIS_ADMIN_WITHDRAW("16","oasis 管理员取出资金"),
+    OASIS_ADMIN_WITHDRAW("16","部落管理员取出资金"),
     VIRTUAL_PRODUCT_ORDER_PAY("17","虚拟商品付款"),
     VIRTUAL_REMITTANCE_TO_SELLER("18","虚拟商品平台汇款到卖家")
 
