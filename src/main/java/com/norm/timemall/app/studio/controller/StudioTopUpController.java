24923aebbe5b9e5b4347c4b555490758419c4966
O08 <47027217+O08@users.noreply.github.com>
2025-10-22 11:23:17 +0800

    code_20251022: bug fix --dan

--- src/main/java/com/norm/timemall/app/studio/controller/StudioTopUpController.java
+++ src/main/java/com/norm/timemall/app/studio/controller/StudioTopUpController.java
@@ -1,8 +1,12 @@
 package com.norm.timemall.app.studio.controller;
 
 import com.norm.timemall.app.base.enums.CodeEnum;
+import com.norm.timemall.app.base.enums.OrderStatusEnum;
+import com.norm.timemall.app.base.exception.ErrorCodeException;
+import com.norm.timemall.app.base.helper.SecurityUserHelper;
 import com.norm.timemall.app.base.pojo.ro.NewOrderRO;
 import com.norm.timemall.app.base.pojo.vo.NewOrderVO;
+import com.norm.timemall.app.base.service.OrderFlowService;
 import com.norm.timemall.app.studio.domain.dto.StudioTopUpDTO;
 import com.norm.timemall.app.studio.service.StudioTopUpService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,13 +20,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudioTopUpController {
     @Autowired
     private StudioTopUpService studioTopUpService;
+    @Autowired
+    private OrderFlowService orderFlowService;
     @ResponseBody
     @PostMapping(value = "/api/v1/web_estudio/order/top_up")
     public NewOrderVO topUp(@Validated @RequestBody StudioTopUpDTO dto){
-        NewOrderRO studioNewOrderRO = studioTopUpService.topUp(dto);
         NewOrderVO vo = new NewOrderVO();
-        vo.setResponseCode(CodeEnum.SUCCESS);
-        vo.setOrder(studioNewOrderRO);
+        try {
+            orderFlowService.insertOrderFlow(SecurityUserHelper.getCurrentPrincipal().getUserId(),
+                    OrderStatusEnum.CREATED.name());
+
+            NewOrderRO studioNewOrderRO = studioTopUpService.topUp(dto);
+            vo.setResponseCode(CodeEnum.SUCCESS);
+            vo.setOrder(studioNewOrderRO);
+
+        }catch (Exception e){
+            throw new ErrorCodeException(CodeEnum.PROCESSING);
+        }finally {
+            orderFlowService.deleteOrderFlow(SecurityUserHelper.getCurrentPrincipal().getUserId(),
+                    OrderStatusEnum.CREATED.name());
+        }
+
+
         return vo;
     }
 
--- src/main/java/com/norm/timemall/app/studio/service/impl/StudioTopUpServiceImpl.java
+++ src/main/java/com/norm/timemall/app/studio/service/impl/StudioTopUpServiceImpl.java
@@ -1,10 +1,8 @@
 package com.norm.timemall.app.studio.service.impl;
 
 import cn.hutool.core.util.IdUtil;
-import com.norm.timemall.app.base.enums.CodeEnum;
 import com.norm.timemall.app.base.enums.FidTypeEnum;
 import com.norm.timemall.app.base.enums.OrderStatusEnum;
-import com.norm.timemall.app.base.exception.ErrorCodeException;
 import com.norm.timemall.app.base.helper.SecurityUserHelper;
 import com.norm.timemall.app.base.mo.Brand;
 import com.norm.timemall.app.base.mo.ProprietaryTradingOrder;
@@ -35,12 +33,8 @@ public class StudioTopUpServiceImpl implements StudioTopUpService {
     private StudioFinAccountMapper studioFinAccountMapper;
     @Override
     public NewOrderRO topUp(StudioTopUpDTO dto) {
-        // verify
         CustomizeUser user = SecurityUserHelper.getCurrentPrincipal();
-        boolean verify =payVerify(user.getUserId(), OrderStatusEnum.CREATING.name());
-        if(!verify){
-            throw new ErrorCodeException(CodeEnum.PROCESSING);
-        }
+
         Brand brand = accountService.findBrandInfoByUserId(user.getUserId());
         // new ProprietaryTradingOrder
         ProprietaryTradingOrder proprietaryTradingOrder=new ProprietaryTradingOrder();
@@ -70,9 +64,4 @@ public class StudioTopUpServiceImpl implements StudioTopUpService {
          studioFinAccountMapper.updateAddDrawableByFid(total,brandId, FidTypeEnum.BRAND.getMark());
     }
 
-    private boolean payVerify(String id,String stage){
-        // verify identity or order exists and debounce
-        return orderFlowService.deleteOrderFlow(id,stage)> 0 ;
-
-    }
 }
